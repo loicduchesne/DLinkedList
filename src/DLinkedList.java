bbdfbfd70ece5a99cabb8ff8ec26b81e467a3e78
loicduchesne <loic402@gmail.com>
2022-10-16 00:56:03 -0400

    added bubbleSort() method

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -69,14 +69,17 @@ public class DLinkedList {
             head = newNode;
             tail = newNode;
         } else {
+            // Using a pointer to point the old tail.
+            SNode pointer = tail;
+
             // Assigning the tail's next to the newNode.
             tail.next = newNode;
 
-            // Assigning newNode's .prev to the current tail.
-            newNode.prev = tail;
-
             // Assigning tail to the new tail (newNode).
             tail = newNode;
+
+            // Assigning newNode's .prev to the current tail.
+            newNode.prev = pointer;
         }
         size ++;
     }
@@ -106,5 +109,92 @@ public class DLinkedList {
         return temp.element;
     }
 
+    /**
+     * This method swaps the elements between two different nodes. Complements switchNode.
+     * @param n1 The first input.
+     * @param n2 The second input.
+     * <br> *Note: the input orders does not matter.
+     */
+    private void swapElements(SNode n1, SNode n2) {
+        if (n1 == null || n2 == null) {
+            throw new IllegalArgumentException("One or both of the input nodes are null.");
+        }
+        // Temporary element pointer to refer to n1's element.
+        Shape temp = n1.element;
 
+        // Swapping the elements.
+        n1.element = n2.element;
+        n2.element = temp;
+    }
+
+    /**
+     * This method sorts the current Linked List by size (from smallest to largest). It uses the private integer size stored in {@link Shape}.
+     * @return Returns true if the bubbleSort loops occurs, returns false if it does not reach the loop segment.
+     * @author Loic Duchesne
+     */
+    public boolean bubbleSort() {
+        // Edge case handling. No sorting if size is 1 or less.
+        if (size <= 1) {
+            return false;
+        }  else {
+            SNode sortedIndex = tail;
+            SNode current = head;
+
+            while (head != sortedIndex) {
+                // When the current node arrives at the end.
+                if (current == sortedIndex) {
+                    current = head;
+                    sortedIndex = sortedIndex.prev;
+                }
+                // Compare element sizes.
+                if (current.element.getSize() > current.next.element.getSize()) {
+                    swapElements(current, current.next);
+                }
+                // Move the current pointer to the next element.
+                current = current.next;
+            }
+            return true;
+        }
+    }
+
+
+    // Print methods & print debugs.
+
+    public void printElements() {
+        SNode pointer = head;
+
+        System.out.println("Elements from 0 to n index:");
+        while (pointer != null) {
+            System.out.println(pointer.element);
+            System.out.println(pointer.element.getShapeName());
+
+            pointer = pointer.next;
+        }
+        System.out.println("----------------------------");
+    }
+
+    public void printElementSizes() {
+        SNode pointer = head;
+
+        System.out.println("Object sizes from 0 to n index:");
+        while (pointer != null) {
+            System.out.println(pointer.element.getSize());
+
+            pointer = pointer.next;
+        }
+        System.out.println("----------------------------");
+    }
+
+    public void debugPrint() {
+        SNode pointer = head;
+
+        System.out.println("----------------------------");
+        while (pointer != null) {
+            System.out.println(pointer.element);
+            System.out.println(pointer.element.getSize());
+
+            pointer = pointer.next;
+        }
+        System.out.println("----------------------------");
+    }
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,15 +1,18 @@
 public class Shape {
     private String shapeName;
+    // Size used for sorting algorithms to determine sorting order.
+    private int size;
 
-    public Shape(String shapeName) {
+    public Shape(String shapeName, int size) {
         this.shapeName = shapeName;
+        this.size = size;
     }
 
     public String getShapeName() {
         return this.shapeName;
     }
 
-    public void changeShapeName(String newName) {
-        this.shapeName = newName;
+    public int getSize() {
+        return this.size;
     }
 }
